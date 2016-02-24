8b1362f05eb628a43f9340a62d5d2ff4e1c5bbb8
rifocamargo <rifocamargo@gmail.com>
2016-02-24 10:15:55 -0300

    Melhoria de tostring

--- TestePETDAO/src/main/java/com/lecom/testepet/entity/Servico.java
+++ TestePETDAO/src/main/java/com/lecom/testepet/entity/Servico.java
@@ -30,138 +30,146 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 @Table(name = "servico")
 public class Servico implements Serializable {
 
-    private static final long serialVersionUID = -3103157719881745331L;
+	private static final long serialVersionUID = -3103157719881745331L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_servico")
-    private Integer idServico;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_servico")
+	private Integer idServico;
 
-    @NotNull
-    @Size(min = 1, max = 45)
-    @Column(name = "nome_servico")
-    private String nomeServico;
+	@NotNull
+	@Size(min = 1, max = 45)
+	@Column(name = "nome_servico")
+	private String nomeServico;
 
-    @NotNull
-    @Size(min = 1, max = 255)
-    @Column(name = "descricao_servico")
-    private String descricaoServico;
+	@NotNull
+	@Size(min = 1, max = 255)
+	@Column(name = "descricao_servico")
+	private String descricaoServico;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "servicoClientePK.servico")
-    private List<ServicoPessoaJuridica> servicoPessoaJuridicaList;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "servicoClientePK.servico")
+	private List<ServicoPessoaJuridica> servicoPessoaJuridicaList;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "servicoClientePK.servico")
-    private List<ServicoPessoaFisica> servicoPessoaFisicaList;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "servicoClientePK.servico")
+	private List<ServicoPessoaFisica> servicoPessoaFisicaList;
 
-    /**
-     * Default Constructor
-     */
-    public Servico() {
-    }
+	/**
+	 * Default Constructor
+	 */
+	public Servico() {
+	}
 
-    /**
-     * @param idServico
-     */
-    public Servico(final Integer idServico) {
-        this.idServico = idServico;
-    }
+	/**
+	 * @param idServico
+	 */
+	public Servico(final Integer idServico) {
+		this.idServico = idServico;
+	}
 
-    /**
-     * @param idServico
-     * @param nomeServico
-     * @param descricaoServico
-     */
-    public Servico(final Integer idServico, final String nomeServico, final String descricaoServico) {
-        this(idServico);
-        this.nomeServico = nomeServico;
-        this.descricaoServico = descricaoServico;
-    }
+	/**
+	 * @param idServico
+	 * @param nomeServico
+	 * @param descricaoServico
+	 */
+	public Servico(final Integer idServico, final String nomeServico, final String descricaoServico) {
+		this(idServico);
+		this.nomeServico = nomeServico;
+		this.descricaoServico = descricaoServico;
+	}
 
-    /**
-     * @param nomeServico
-     * @param descricaoServico
-     */
-    public Servico(final String nomeServico, final String descricaoServico) {
-        this.nomeServico = nomeServico;
-        this.descricaoServico = descricaoServico;
-    }
+	/**
+	 * @param nomeServico
+	 * @param descricaoServico
+	 */
+	public Servico(final String nomeServico, final String descricaoServico) {
+		this.nomeServico = nomeServico;
+		this.descricaoServico = descricaoServico;
+	}
 
-    /**
-     * @return the idServico
-     */
-    public Integer getIdServico() {
-        return idServico;
-    }
+	/**
+	 * @return the idServico
+	 */
+	public Integer getIdServico() {
+		return idServico;
+	}
 
-    /**
-     * @param idServico the idServico to set
-     */
-    public void setIdServico(final Integer idServico) {
-        this.idServico = idServico;
-    }
+	/**
+	 * @param idServico
+	 *            the idServico to set
+	 */
+	public void setIdServico(final Integer idServico) {
+		this.idServico = idServico;
+	}
 
-    /**
-     * @return the nomeServico
-     */
-    public String getNomeServico() {
-        return nomeServico;
-    }
+	/**
+	 * @return the nomeServico
+	 */
+	public String getNomeServico() {
+		return nomeServico;
+	}
 
-    /**
-     * @param nomeServico the nomeServico to set
-     */
-    public void setNomeServico(final String nomeServico) {
-        this.nomeServico = nomeServico;
-    }
+	/**
+	 * @param nomeServico
+	 *            the nomeServico to set
+	 */
+	public void setNomeServico(final String nomeServico) {
+		this.nomeServico = nomeServico;
+	}
 
-    /**
-     * @return the descricaoServico
-     */
-    public String getDescricaoServico() {
-        return descricaoServico;
-    }
+	/**
+	 * @return the descricaoServico
+	 */
+	public String getDescricaoServico() {
+		return descricaoServico;
+	}
 
-    /**
-     * @param descricaoServico the descricaoServico to set
-     */
-    public void setDescricaoServico(final String descricaoServico) {
-        this.descricaoServico = descricaoServico;
-    }
+	/**
+	 * @param descricaoServico
+	 *            the descricaoServico to set
+	 */
+	public void setDescricaoServico(final String descricaoServico) {
+		this.descricaoServico = descricaoServico;
+	}
 
-    public List<ServicoPessoaJuridica> getServicoPessoaJuridicaList() {
-        return servicoPessoaJuridicaList;
-    }
+	public List<ServicoPessoaJuridica> getServicoPessoaJuridicaList() {
+		return servicoPessoaJuridicaList;
+	}
 
-    public void setServicoPessoaJuridicaList(final List<ServicoPessoaJuridica> servicoPessoaJuridicaList) {
-        this.servicoPessoaJuridicaList = servicoPessoaJuridicaList;
-    }
+	public void setServicoPessoaJuridicaList(final List<ServicoPessoaJuridica> servicoPessoaJuridicaList) {
+		this.servicoPessoaJuridicaList = servicoPessoaJuridicaList;
+	}
 
-    public List<ServicoPessoaFisica> getServicoPessoaFisicaList() {
-        return servicoPessoaFisicaList;
-    }
+	public List<ServicoPessoaFisica> getServicoPessoaFisicaList() {
+		return servicoPessoaFisicaList;
+	}
 
-    public void setServicoPessoaFisicaList(final List<ServicoPessoaFisica> servicoPessoaFisicaList) {
-        this.servicoPessoaFisicaList = servicoPessoaFisicaList;
-    }
+	public void setServicoPessoaFisicaList(final List<ServicoPessoaFisica> servicoPessoaFisicaList) {
+		this.servicoPessoaFisicaList = servicoPessoaFisicaList;
+	}
 
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder().append(this.idServico).hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this.idServico).hashCode();
+	}
 
-    @Override
-    public boolean equals(final Object object) {
-        if (!(object instanceof Servico)) {
-            return false;
-        }
-        final Servico other = (Servico) object;
+	@Override
+	public boolean equals(final Object object) {
+		if (!(object instanceof Servico)) {
+			return false;
+		}
+		final Servico other = (Servico) object;
 
-        return new EqualsBuilder().append(this.idServico, other.idServico).isEquals();
-    }
+		return new EqualsBuilder().append(this.idServico, other.idServico).isEquals();
+	}
 
-    @Override
-    public String toString() {
-        return "Servico[ idServico=" + idServico + " ]";
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Servico [idServico=" + idServico + ", nomeServico=" + nomeServico + "]";
+	}
 
 }
